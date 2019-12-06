ec3837c6427960757c423be4c21dc027c42ec427
DaianaVargas <dai.vgsg@gmail.com>
2019-12-06 13:19:32 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+/* eslint-disable */
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="message"/>
+    <input type="text" v-model="message"/>
+    <button @click="alertMessage">Alerta</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      message: 'Hello, Dai!',
+    };
+  },
+  methods: {
+    alertMessage() {
+      alert(this.message);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
